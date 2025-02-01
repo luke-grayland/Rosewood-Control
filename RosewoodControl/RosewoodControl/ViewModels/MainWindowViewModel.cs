7bb931616ff44e20dd222b9b66a2f01b6e95652d
Luke Grayland <lgrayland96@gmail.com>
2025-02-01 12:20:42 +0000

    override of MVVM template

--- RosewoodControl/RosewoodControl/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace RosewoodControl.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- RosewoodControl/RosewoodControl/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace RosewoodControl.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
