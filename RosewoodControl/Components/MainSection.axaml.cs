e336aa587bc4fdb4a2025b9255c90082b40f41a5
Luke Grayland <lgrayland96@gmail.com>
2025-02-01 14:26:16 +0000

    built out layout, added gain panel

---
+++ RosewoodControl/Components/MainSection.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RosewoodControl.Components;
+
+public partial class MainSection : UserControl
+{
+    public MainSection()
+    {
+        InitializeComponent();
+    }
+}
