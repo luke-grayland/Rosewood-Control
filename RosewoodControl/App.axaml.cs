53d87c2d543eb32e16ea3c0090e099435a183d87
Luke Grayland <lgrayland96@gmail.com>
2025-02-02 18:23:26 +0000

    got signal r connection working

--- RosewoodControl/App.axaml.cs
+++ RosewoodControl/App.axaml.cs
@@ -1,9 +1,13 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia.Markup.Xaml;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 using RosewoodControl.ViewModels;
 using RosewoodControl.Views;
 
@@ -11,6 +15,8 @@ namespace RosewoodControl;
 
 public partial class App : Application
 {
+    public static IServiceProvider Services { get; private set; }
+    
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -18,10 +24,12 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        Services = serviceCollection.BuildServiceProvider();
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
@@ -32,6 +40,36 @@ public partial class App : Application
         base.OnFrameworkInitializationCompleted();
     }
 
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<HubConnection>(provider =>
+        {
+            try
+            {
+                Console.WriteLine("Attempting to connect to hub");
+                var connection = new HubConnectionBuilder()
+                    .WithUrl("https://localhost:7181/DeviceHub")
+                    .WithAutomaticReconnect()
+                    .Build();
+
+                connection.Reconnecting += (error) =>
+                {
+                    Console.WriteLine("Reconnecting...");
+                    return Task.CompletedTask;
+                };
+                
+                connection.StartAsync();
+                Console.WriteLine("Connected to hub");
+                return connection;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to connect to hub: {e.Message}");
+                throw;
+            }
+        });
+    }
+    
     private void DisableAvaloniaDataAnnotationValidation()
     {
         // Get an array of plugins to remove
--- RosewoodControl/Views/Components/MainSection.axaml.cs
+++ RosewoodControl/Views/Components/MainSection.axaml.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RosewoodControl.Views.Components;
 
 public partial class MainSection : UserControl
 {
+    private readonly HubConnection _hubConnection;
+    
     public MainSection()
     {
         InitializeComponent();
+        _hubConnection = App.Services.GetRequiredService<HubConnection>();
     }
 
     private void GainSlider_OnPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
@@ -17,6 +23,16 @@ public partial class MainSection : UserControl
         {
             newGainSliderVal = Math.Round(newGainSliderVal, 2);
             Console.WriteLine(newGainSliderVal);
+            
+            Task.Run(async () =>
+            {
+                if (_hubConnection.State != HubConnectionState.Connected)
+                {
+                    Console.WriteLine("No active connection");
+                    return;
+                }
+                await _hubConnection.InvokeAsync("SendCommand", "Pi123", newGainSliderVal.ToString());
+            });
         }
     }
 }
