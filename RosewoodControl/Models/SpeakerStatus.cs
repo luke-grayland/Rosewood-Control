44db2051014fd1d5a9019f2ee03a77e876d6e66b
Luke Grayland <lgrayland96@gmail.com>
2025-02-05 21:33:41 +0000

    started adding speaker control

---
+++ RosewoodControl/Models/SpeakerStatus.cs
@@ -0,0 +1,8 @@
+namespace RosewoodControl.Models;
+
+public enum SpeakerStatus
+{
+    Active,
+    Blown,
+    Offline
+}
---
+++ RosewoodControl/Views/Components/SpeakerControl.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls.Primitives;
+using RosewoodControl.Models;
+
+namespace RosewoodControl.Views.Components;
+
+public class SpeakerControl : TemplatedControl
+{
+    public SpeakerStatus Status { get; set; }
+}
